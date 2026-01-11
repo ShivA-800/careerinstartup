9b5df114997bfabcacea57c8abf705b897a2550a
ShivA-800 <6688shivaradharapu039@gmail.com>
2026-01-11 12:13:17 +0530

    feat: add Google Analytics 4 tracking (G-R8N2XJ6Y19)

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,40 @@
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { BrowserRouter, Routes, Route, useLocation } from 'react-router-dom';
+import { useEffect } from 'react';
 import Home from './pages/Home';
 import AdminLogin from './pages/AdminLogin';
 import AdminDashboard from './pages/AdminDashboard';
 import Terms from './pages/Terms';
 import Privacy from './pages/Privacy';
 
+// Track pageviews for SPA route changes
+function usePageviews() {
+  const location = useLocation();
+  useEffect(() => {
+    if (typeof window.gtag !== 'undefined') {
+      window.gtag('event', 'page_view', {
+        page_path: location.pathname + location.search,
+      });
+    }
+  }, [location]);
+}
+
+function AppRoutes() {
+  usePageviews();
+  return (
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/secure-admin-login" element={<AdminLogin />} />
+      <Route path="/admin/dashboard" element={<AdminDashboard />} />
+      <Route path="/terms" element={<Terms />} />
+      <Route path="/privacy" element={<Privacy />} />
+    </Routes>
+  );
+}
+
 function App() {
   return (
     <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/secure-admin-login" element={<AdminLogin />} />
-        <Route path="/admin/dashboard" element={<AdminDashboard />} />
-        <Route path="/terms" element={<Terms />} />
-        <Route path="/privacy" element={<Privacy />} />
-      </Routes>
+      <AppRoutes />
     </BrowserRouter>
   );
 }
